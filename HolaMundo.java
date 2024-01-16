86a088004678325afcaa3164215394cfb7423d32
Sam Flores <samfloresaguirre@gmail.com>
2024-01-16 17:00:48 -0600

    Ingreso de codigo Java

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo en Java!");
+        
+        // Modificación extra: Saludo personalizado
+        String nombre = "Usuario";
+        System.out.println("¡Hola " + nombre + "!");
+    }
+}
