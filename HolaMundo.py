b1a7b709dcda9c7d5a551133598a34cfe9db1237
Sam Flores <samfloresaguirre@gmail.com>
2024-01-16 17:09:57 -0600

    [MOD] Modificacion de Linea 5

--- HolaMundo.py
+++ HolaMundo.py
@@ -2,7 +2,7 @@ print("¡Hola Mundo en Python!")
 fecha = "16 de enero de 2024"
 
 nom = "Usuario"
-print("¡Hola " + nombre + "!")
+print("¡Hola "nombre + "!")
 
 print("La fecha es de hoy es: " + Fecha)
 
