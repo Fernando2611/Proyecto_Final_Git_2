f66e5aeaa0da95d185562a42d7fba8ee0b1963fd
Sam Flores <samfloresaguirre@gmail.com>
2024-01-16 17:35:07 -0600

    (Java) Modificacion del codigo en linea 3

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo en Java!");
+        System.out.println("¡Hola Mundo en Java!")
         
         // Modificación extra: Saludo personalizado
         String nom = "Usuario";
