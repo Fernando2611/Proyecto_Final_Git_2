5612caeeb7dd4dffabaa4f23b3284f448b3d83d6
Fernando <fernandonm@ciencias.unam.mx>
2024-02-01 18:57:59 -0600

    [MOD] Correguí errores en el commit con nombre Renombrar variable nombre a nom en HolaMundo.py

--- HolaMundo.py
+++ HolaMundo.py
@@ -2,7 +2,6 @@ print("¡Hola Mundo en Python!")
 fecha = "16 de enero de 2024"
 
 nom = "Usuario"
-print("¡Hola "nombre + "!")
+print("¡Hola "+ nom + "!")
 
-print("La fecha es de hoy es: " + Fecha)
-
+print("La fecha es de hoy es: " + fecha)
